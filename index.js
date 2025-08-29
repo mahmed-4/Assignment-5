6d1fa6236c05e9faa2d13a931f972483cdd84068
mahmed-4 <masumaa16@gmail.com>
2025-08-29 23:55:14 +0600

    fix: remove duplicate alert in copy button

--- index.js
+++ index.js
@@ -9,24 +9,22 @@ for (let i = 0; i < heartIcons.length; i++) {
 }
 
 // ............... call btn ...............
-const btnCalls = document.querySelectorAll('.btn-call');
+const btnCalls = document.querySelectorAll(".btn-call");
 
-for(const btn of btnCalls){
-
+for (const btn of btnCalls) {
   btn.addEventListener("click", function () {
-
     const parentCard = this.closest(".card");
     const text = parentCard.querySelector("p.text-sm").innerText;
     const callNumber = parentCard.querySelector(".phone-number").innerText;
 
-    let coins = Number(document.getElementById('coins').innerText);
-    
-    if (coins >= 20){
+    let coins = Number(document.getElementById("coins").innerText);
+
+    if (coins >= 20) {
       alert(`Calling ${text} ${callNumber}....`);
       document.getElementById("coins").innerText = coins - 20;
 
-      const container = document.getElementById('history-container');
-      const div = document.createElement('div');
+      const container = document.getElementById("history-container");
+      const div = document.createElement("div");
       div.innerHTML = `
       <div class="flex gap-1">
           <div>
@@ -37,43 +35,45 @@ for(const btn of btnCalls){
             <p>11:36:58 AM</p>
           </div>
         </div>
-      `
+      `;
       container.appendChild(div);
     } else {
-      alert("You Have Not Sufficient Balance. You Need 20 Coins To Call.")
+      alert("You Have Not Sufficient Balance. You Need 20 Coins To Call.");
     }
   });
 }
 
 // ............... clear btn ...............
-document.getElementById('clear-btn').addEventListener('click', function(){
-  document.getElementById('history-container').innerText = '';
-})
+document.getElementById("clear-btn").addEventListener("click", function () {
+  document.getElementById("history-container").innerText = "";
+});
 
 // ............... copy btn ...............
-const copyBtns = document.getElementsByClassName('copy-btn');
+const copyBtns = document.getElementsByClassName("copy-btn");
 
 let displayCopyCount = 0;
-for(const btn of copyBtns){
-  btn.addEventListener('click', function(){
-    const copyCount = Number(document.getElementById('copy-count').innerText);
-    
+for (const btn of copyBtns) {
+  btn.addEventListener("click", function () {
+    const copyCount = Number(document.getElementById("copy-count").innerText);
+
     const parentCard = this.closest(".card");
-    const callNumber = parentCard.querySelector(".phone-number").innerText;   
-// ok 
-navigator.clipboard.writeText(callNumber).then(() => {
-  alert(`Number ${callNumber} copied to clipboard`);
-})
-.catch(err => {
-  alert(`Failed to copy: `, err);
-})
+    const callNumber = parentCard.querySelector(".phone-number").innerText;
 
-// -----
-    alert(`number is copied ${callNumber}`);
+    navigator.clipboard
+      .writeText(callNumber)
+      .then(() => {
+        alert(`Number ${callNumber} copied to clipboard`);
+      })
+      .catch((err) => {
+        alert(`Failed to copy: `, err);
+      });
 
-    displayCopyCount++
-    document.getElementById('copy-count').innerText = displayCopyCount;
-  })
+  
+    // alert(`number is copied ${callNumber}`);
+
+    displayCopyCount++;
+    document.getElementById("copy-count").innerText = displayCopyCount;
+  });
 }
 
 // ---- call time -----
@@ -82,4 +82,4 @@ function showTime() {
   document.getElementById("clock").innerText = now.toLocaleTimeString();
 }
 setInterval(showTime, 1000);
-showTime() ;
+showTime();
